ee00c0bb19c010d217bbe98fe08a3461e1c54a51
Andriy-Lesyk <74345435+Andriy-Lesyk@users.noreply.github.com>
2021-04-28 10:11:45 +0300

    Завершает ДЗ

--- index.js
+++ index.js
@@ -10,19 +10,8 @@ const markup =menus.map(dishName).join('')
 console.log(markup)
 menuList.insertAdjacentHTML('beforeend', markup)
 
-const ingredientList = menuList.querySelectorAll('.tag-list')
-console.log(ingredientList)
-
-
-
-
-
-
-
 const changeTheme = document.querySelector('.theme-switch__toggle')
 const bodyPr = document.querySelector('body')
-console.log(changeTheme)
-console.log(bodyPr)
 
 changeTheme.addEventListener('change', onDarkTheme)
 
@@ -34,7 +23,15 @@ function onDarkTheme (){
     bodyPr.classList.toggle(Theme.DARK)
     if (changeTheme.checked===true){
         localStorage.setItem('theme', Theme.DARK)
+        
     }else{
-        localStorage.clear();
+        localStorage.removeItem('theme');
     }
+}
+
+if(localStorage.theme==='dark-theme' ){
+bodyPr.classList.add(localStorage.getItem('theme'));
+changeTheme.checked=true
+}else{
+    changeTheme.checked=false
 }
